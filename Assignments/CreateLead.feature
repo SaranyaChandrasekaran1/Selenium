2ac9e9f0587d2569fb74aec3c141b4b75a2dda9e
SaranyaChandrasekaran1 <88572359+SaranyaChandrasekaran1@users.noreply.github.com>
2021-09-14 16:35:00 +0530

    Add files via upload

---
+++ Assignments/CreateLead.feature
@@ -0,0 +1,21 @@
+Feature: Login Functionality of LeafTaps application
+
+
+
+Scenario:
+#Given Open the chrome browser
+#And Load the application url 'http://leaftaps.com/opentaps/control/login'
+
+
+Given Enter the username as 'Democsr'
+And Enter the password as 'crmsfa'
+When Click on Login button
+Then Homepage should be displayed
+When click on 'CRM/SFA' link
+Then 'My Home' page should be displayed
+When click on 'Leads' link
+Then 'My Leads' page should be displayed
+When click on 'Create Lead' link
+Then 'Create Lead' page should be displayed
+
+
---
+++ Assignments/HooksImplementation.java
@@ -0,0 +1,30 @@
+package steps;
+
+import java.time.Duration;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java.After;
+import io.cucumber.java.Before;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HooksImplementation extends BaseClass {
+	@Before
+	public void precondition()
+	{
+		WebDriverManager.chromedriver().setup(); 
+		  driver = new ChromeDriver();
+		 driver.manage().window().maximize();
+		  driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		  driver.get("http://leaftaps.com/opentaps/control/login");
+	}
+	
+	@After
+	public void postcondition()
+	{
+		
+		driver.close();
+	}
+	
+
+}
